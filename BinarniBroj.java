8789d39feeeb67b83843a3cf10daf02ba63f057a
Gordan Sajević <gsajevic@hotmail.com>
2014-11-29 10:43:06 +0100

    Create BinarniBroj.java

---
+++ BinarniBroj.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+
+public class BinarniBroj {
+	
+	/**
+	 * Funkcija prima cijeli broj i vraća njegov binarni broj u obliku stringa
+	 * @param broj
+	 * @return okreni
+	 */
+	
+	private static String pretvoriUBinarni(int broj) {
+		String binarni = "";
+		String okreni="";
+		if (broj > 0)
+		{
+			while (broj != 0)
+			{
+				if (broj % 2 == 0)
+				{
+					binarni += "0";
+				}
+				else
+				{
+					binarni += "1"; 
+				}
+				broj = broj/2;
+			}
+		}
+		else
+		{
+			while (broj != 0)
+			{
+				if (broj % 2 == 0)
+				{
+					binarni += "1";
+				}
+				else
+				{
+					binarni += "0"; 
+				}
+				broj = broj/2;
+			}
+		}
+			
+		for (int i=binarni.length()-1; i>=0; i--)
+		{
+			okreni += binarni.charAt(i);
+		}
+		return okreni;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Unesite jedan broj(do 255): ");
+		Scanner unos = new Scanner(System.in);
+		int broj = unos.nextInt();
+		String binarni = pretvoriUBinarni(broj);
+		System.out.println(binarni);
+	}
+
+}
